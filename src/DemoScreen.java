be1f553ad6d3530cb66577ffeb816ba9c4b02f29
NguyenQuang179 <79895324+NguyenQuang179@users.noreply.github.com>
2022-12-01 15:09:14 +0700

    Initial Demo Login Screen
    - Basic Login GUI

---
+++ src/DemoScreen.java
@@ -0,0 +1,118 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JPasswordField;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextPane;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JDesktopPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.JEditorPane;
+import java.awt.Color;
+
+public class DemoScreen extends JFrame {
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DemoScreen frame = new DemoScreen();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public DemoScreen() {
+		setResizable(false);
+		setTitle("Demo");
+		getContentPane().setEnabled(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 720, 480);
+		getContentPane().setLayout(null);
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		desktopPane.setBackground(new Color(255, 255, 255));
+		desktopPane.setBounds(125, 46, 455, 350);
+		getContentPane().add(desktopPane);
+		
+		JLabel lblNewLabel = new JLabel("Tên đăng nhập:");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel.setBounds(87, 134, 120, 28);
+		desktopPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setHorizontalAlignment(SwingConstants.LEFT);
+		textField.setColumns(10);
+		textField.setBounds(217, 134, 186, 28);
+		desktopPane.add(textField);
+		
+		JLabel lblNewLabel_1 = new JLabel("ĐĂNG NHẬP");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 24));
+		lblNewLabel_1.setBounds(129, 49, 192, 53);
+		desktopPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Mật khẩu:");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_2.setBounds(131, 171, 76, 28);
+		desktopPane.add(lblNewLabel_2);
+		
+		passwordField = new JPasswordField();
+		passwordField.setHorizontalAlignment(SwingConstants.LEFT);
+		passwordField.setBounds(217, 171, 186, 28);
+		desktopPane.add(passwordField);
+		
+		JButton btnNewButton = new JButton("Đăng Nhập");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnNewButton.setBounds(87, 246, 125, 51);
+		desktopPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Đăng Ký");
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnNewButton_1.setBounds(278, 246, 125, 51);
+		desktopPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Quên mật khẩu?");
+		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton_2.setBackground(Color.WHITE);
+		btnNewButton_2.setBorder(null);
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_2.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnNewButton_2.setBounds(290, 209, 112, 21);
+		desktopPane.add(btnNewButton_2);
+	}
+}
