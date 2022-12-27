7de93300b3de16ce51255f3e8f5cec005bd0ef3e
Hoang Minh <hoangminhbadao@gmail.com>
2022-12-27 20:00:41 +0700

    Redo CreateGroup
    Bỏ CreateGroup_Friendlist, thêm friendlist lại vào CreateGroup

--- src/CreateGroup.java
+++ src/CreateGroup.java
@@ -9,6 +9,7 @@ import javax.swing.border.SoftBevelBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -31,13 +32,13 @@ import javax.swing.JButton;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.DefaultListModel;
+import java.awt.Color;
+
 
 public class CreateGroup extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
-	private JTextField textField_1;
 	
 	private String _username;
 	private Socket _clientSocket;
@@ -47,7 +48,16 @@ public class CreateGroup extends JFrame {
 	
 	JLabel lblNewLabel = new JLabel("Create Group");
 	JScrollPane scrollPane_friendlist = new JScrollPane();
-	JList<String> list_friend = new JList<>();
+	JList<String> list_member = new JList<>();
+	JButton btnKickMember = new JButton("Kick");
+	JButton btnCreateGroup = new JButton("Create");
+	private final JScrollPane scrollPane_friendlist_1 = new JScrollPane();
+	private final JList<String> list_friend = new JList<String>();
+	private final JButton btnAddMember = new JButton("Add");
+	private final JLabel lblNewLabel_1_2 = new JLabel("Member:");
+	private final JLabel lblNewLabel_1_2_1 = new JLabel("Friend List:");
+	private final JLabel lblNewLabel_1_1 = new JLabel("*The person who created the group will automatically be the admin");
+	
 	/**
 	 * Launch the application.
 	 */
@@ -74,7 +84,7 @@ public class CreateGroup extends JFrame {
 		this._pw = pw;
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 550, 600);
+		setBounds(100, 100, 572, 660);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -82,20 +92,69 @@ public class CreateGroup extends JFrame {
 		contentPane.setLayout(null);
 		
 		
-		lblNewLabel.setBounds(167, 11, 202, 37);
+		lblNewLabel.setBounds(178, 11, 202, 37);
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 30));
 		contentPane.add(lblNewLabel);
 		
 		
-		scrollPane_friendlist.setBounds(288, 68, 238, 302);
+		scrollPane_friendlist.setBounds(20, 161, 238, 302);
 		contentPane.add(scrollPane_friendlist);
 		
 		
-		scrollPane_friendlist.setViewportView(list_friend);
-		list_friend.setBackground(UIManager.getColor("CheckBox.highlight"));
-		list_friend.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		list_friend.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		scrollPane_friendlist.setViewportView(list_member);
+		list_member.setBackground(UIManager.getColor("CheckBox.highlight"));
+		list_member.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		list_member.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		
+		
+		list_member.setSelectionModel(new DefaultListSelectionModel() {
+		    @Override
+		    public void setSelectionInterval(int index0, int index1) {
+		        if(super.isSelectedIndex(index0)) {
+		            super.removeSelectionInterval(index0, index1);
+		        }
+		        else {
+		            super.addSelectionInterval(index0, index1);
+		        }
+		    }
+		});
+		
+		list_member.setModel(new AbstractListModel() {
+			String[] values = new String[] {};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		
+		btnCreateGroup.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnCreateGroup.setBounds(178, 568, 202, 44);
+		contentPane.add(btnCreateGroup);
+		
+		btnKickMember.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnKickMember.setBounds(20, 474, 238, 44);
+		contentPane.add(btnKickMember);
+		
+		textField = new JTextField();
+		textField.setBounds(20, 85, 200, 25);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Group name:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_1.setBounds(20, 59, 200, 20);
+		contentPane.add(lblNewLabel_1);
+		
+		scrollPane_friendlist_1.setBounds(300, 161, 238, 302);
+		
+		contentPane.add(scrollPane_friendlist_1);
+		list_friend.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		list_friend.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		list_friend.setBackground(UIManager.getColor("CheckBox.highlight"));
+		
+		scrollPane_friendlist_1.setViewportView(list_friend);
 		
 		list_friend.setSelectionModel(new DefaultListSelectionModel() {
 		    @Override
@@ -119,54 +178,66 @@ public class CreateGroup extends JFrame {
 			}
 		});
 		
-		//Show friendlist
-		
-		
-		
-		JButton btnCreateGroup = new JButton("Create");
-		btnCreateGroup.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnCreateGroup.setBounds(167, 508, 202, 44);
-		contentPane.add(btnCreateGroup);
-		
-		JButton btnAddMember = new JButton("Add member");
-		
 		btnAddMember.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnAddMember.setBounds(288, 381, 238, 44);
+		btnAddMember.setBounds(300, 474, 238, 44);
+		
 		contentPane.add(btnAddMember);
+		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_1_2.setBounds(20, 130, 200, 20);
 		
-		textField = new JTextField();
-		textField.setBounds(10, 94, 200, 25);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		contentPane.add(lblNewLabel_1_2);
+		lblNewLabel_1_2_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_1_2_1.setBounds(300, 130, 200, 20);
 		
-		JLabel lblNewLabel_1 = new JLabel("Group name:");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNewLabel_1.setBounds(10, 68, 200, 20);
-		contentPane.add(lblNewLabel_1);
+		contentPane.add(lblNewLabel_1_2_1);
 		
-		JLabel lblNewLabel_1_1 = new JLabel("Group name:");
-		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNewLabel_1_1.setBounds(10, 130, 200, 20);
+		lblNewLabel_1_1.setForeground(new Color(128, 128, 128));
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.ITALIC, 14));
+		lblNewLabel_1_1.setBounds(20, 529, 518, 20);
+		
 		contentPane.add(lblNewLabel_1_1);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(10, 156, 200, 25);
-		contentPane.add(textField_1);
-		
-		_readThread = new ClientReaderThread(socket, list_friend,_username);
-		_readThread.start();
+//		_readThread = new ClientReaderThread(socket, list_friend,_username);
+//		_readThread.start();
 		
 		//list.setBounds(425, 84, 1, 1);
 		//contentPane.add(list);
 		
 		
 		//--------------------EVENT--------------------
+		_readThread = new ClientReaderThread(socket, list_friend, username);
+		_readThread.start();
+			
+		DefaultListModel model_member = (DefaultListModel) list_member.getModel();
+		DefaultListModel model_friend = (DefaultListModel) list_friend.getModel();
+		
+		btnKickMember.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int selectedIndex = list_member.getSelectedIndex();
+				String selected_value = list_member.getSelectedValue();
+				if (selectedIndex != -1) {
+				    model_member.remove(selectedIndex);
+				    
+				}
+				else {
+					JOptionPane.showMessageDialog(contentPane, "Please choose someone!",
+				               "Warning", JOptionPane.WARNING_MESSAGE);
+				}
+			}
+		});
+		
 		btnAddMember.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				CreateGroup_Friendlist friendlist_form = new CreateGroup_Friendlist(socket, pw,  username, list_friend);
-				friendlist_form.setVisible(true);
-			
+				int selectedIndex = list_friend.getSelectedIndex();
+				String selected_value = list_friend.getSelectedValue();
+				if (selectedIndex != -1) {
+				    model_friend.remove(selectedIndex);
+
+				}
+				else {
+					JOptionPane.showMessageDialog(contentPane, "Please choose someone!",
+				               "Warning", JOptionPane.WARNING_MESSAGE);
+				}
 			}
 		});
 		
