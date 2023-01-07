cef5d4f6194e99c0aa5dde3ec325e90f7d76acac
Hoang Minh <hoangminhbadao@gmail.com>
2023-01-08 02:48:36 +0700

    Revert "minor adjustment (sleep)"
    This reverts commit cbaeb0f28f7c7a52cd628a13196c01676191fe62.

--- src/ThreadServer.java
+++ src/ThreadServer.java
@@ -729,7 +729,7 @@ public class ThreadServer extends Thread {
 							
 						}
 						else {
-							server.sendMessageToAUser(senderThread,"grant_admin-Error: Input error!");
+							server.sendMessageToAUser(senderThread,"grant_admin-Error: User not found!");
 						}
 					} catch (SQLException e) {
 						// TODO Auto-generated catch block
